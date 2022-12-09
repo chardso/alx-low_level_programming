6cd0ed798047a3a1d9050a93880afd70e785270b
chardso <wafelaandrew2@gmail.com>
2022-12-10 01:59:02 +0300

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main:Entry to all programs
+ * return:Always zero when successful
+ */
+ int main(void);
+{
+printf("with a proper grammer,but the outcome is a piece of art\n");
+return(0);
+}
+

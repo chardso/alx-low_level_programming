ce9c291c408a0eba411bfca8d7590f92a0f08a40
chardso <wafelaandrew2@gmail.com>
2022-12-10 01:13:21 +0300

    my first line in C programing

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry for all programs
+ * return: Always zero when successfully run
+ */
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzle");
+return (0);
+}
+

7745a4a42620efc6bd8400587aff04ff3b2310f1
chardso <wafelaandrew2@gmail.com>
2022-12-11 22:53:39 +0300

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Entry path
+* Return: 0 when success
+*/
+int main(void)
+{
+char a2z;
+for (a2z = 97; a2z <= 122; a2z++)
+{
+putchar(a2z);
+}
+putchar('\n');
+return (0);
+}
+

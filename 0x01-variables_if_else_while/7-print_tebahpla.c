a59039bf5db49c7beb4049659fdf032288f6c071
chardso <wafelaandrew2@gmail.com>
2022-12-11 23:38:04 +0300

    print tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry path
+ * Return: 0 when success
+*/
+int main(void)
+{
+char a2z;
+for (a2z = 122; a2z >= 97; a2z--)
+{
+putchar(a2z);
+}
+putchar(10);
+return (0);
+}
+

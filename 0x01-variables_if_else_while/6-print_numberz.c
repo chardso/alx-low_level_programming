9170faa3ee486925e2a3407bdc60ca4ec4ce4283
chardso <wafelaandrew2@gmail.com>
2022-12-11 23:32:01 +0300

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Entry path
+* Return: 0 when success
+*/
+int main(void)
+{
+
+int nums;
+/*
+* All numbers in ASII 48 to 57
+*/
+for (nums = 48; nums <= 57; nums++)
+{
+putchar(nums);
+}
+putchar('\n');
+return (0);
+}
+

a79c1345a1139575d92d3210df853282f523919e
chardso <wafelaandrew2@gmail.com>
2022-12-11 16:31:20 +0300

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main - Entry for all programs
- *
- * return: Always zero when successfully run
- */
+* main - Entry for all programs
+*
+* return: Always zero when successfully run
+*/
 int main(void)
- {
- puts("\"programming is like building a multilingual puzzle");
- return(0);
- }
+{
+puts("\"programming is like building a multilingual puzzle");
+return(0);
+}
 

435ae7e94c2fd63b065760d050bf8d63db5cbd2e
chardso <wafelaandrew2@gmail.com>
2022-12-12 01:06:20 +0300

    print comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return:  0 when successful
+*/
+int main(void)
+{
+int num;
+for (num = 48; num <= 57; num++)
+}
+putchar(num);
+if (num <  57)
+{
+putchar(44);
+putchar(' ');
+{
+putchar(10);
+return (0);
+}
+

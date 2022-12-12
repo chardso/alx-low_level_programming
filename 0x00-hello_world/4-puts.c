6d5a790aad29a1add7264b6226fba82f9819990c
chardso <wafelaandrew2@gmail.com>
2022-12-13 02:10:31 +0300

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+
 /**
-* main - Entry point for all programs
-*
-* return: Always returns zero when, the program is successful
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
 

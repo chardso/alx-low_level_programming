f32d01061a347b2258b4336f76cb0df7a9ae0b4f
chardso <wafelaandrew2@gmail.com>
2022-12-12 04:01:58 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,17 +5,17 @@
 * retun: Always 0 (success)
 */
 int main(void)
-           {
+{
 	     int a;
              for(a = 0; a < 10; a++) 
-             {
+{
 	putchar(a +'0');
 	if(a < 9) 
-                 { 
+{ 
 	           putchar(',');
-	             putchar(',');
-                }
+	           putchar(',');
+}
                  putchar ('\n');
-                return (0);
-          }
+                 return (0);
+}
 	

7c606395c662d536b9fef1046f68b88f75a037b1
chardso <wafelaandrew2@gmail.com>
2022-12-12 03:06:53 +0300

    print comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 /**
 * main - Entry point
-* Return:  0 when successful
+*
+* retun: Always 0 (success)
 */
 int main(void)
 {
-int num;
-for (num = 48; num <= 57; num++)
+	int a;
+for (a = 0; a < 10; a++) 
+{
+	putchar (a +'0');
+	if (a < 9) 
+{ 
+	putchar (',');
+	putchar (',');
 }
-putchar(num);
-if (num <  57)
-{
-putchar(44);
-putchar(' ');
-{
-putchar(10);
+}
+putchar ('\n');
 return (0);
 }
-
+	

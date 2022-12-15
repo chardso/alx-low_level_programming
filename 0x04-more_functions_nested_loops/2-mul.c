abe519d4cf4654666e7f9bb114f4ef632b208013
chardso <wafelaandrew2@gmail.com>
2022-12-16 01:59:23 +0300

    task 3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* mul - returns the product of c and s
+* Return: 0
+* @c: holds the value of first number passed in
+* @s: holds the value of second number passed in
+* Checks if character is upper case
+*/
+int mul(int c, int s)
+{
+return (c * s);
+}
+

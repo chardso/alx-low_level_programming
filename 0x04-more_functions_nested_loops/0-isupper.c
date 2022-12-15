3e2356eb49447776b79f6bc9e28e54ee944d0035
chardso <wafelaandrew2@gmail.com>
2022-12-16 01:15:58 +0300

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper - returns 1 if upper case else 0
+ * Return: 0
+ * @c: is to passed into the function
+ * Checks if character is upper case
+*/
+int _isupper(int c)
+{
+		if isupper(c)
+		{
+				return (1);
+		}
+		else
+		{
+			return (0);
+		}
+}
+

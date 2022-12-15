1c4809653efe0b6e052cabec248972b084273308
chardso <wafelaandrew2@gmail.com>
2022-12-16 02:04:09 +0300

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_numbers - prints out all numbers from 0 -> 9
+* Return: void
+* Checks if character is upper case
+*/
+void print_numbers(void)
+{
+int a;
+for (a = 48; a <= 57; a++)
+{
+_putchar(a);
+}
+_putchar(10);
+}
+

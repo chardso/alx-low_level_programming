552f05a529eb6d79b2dfc649ef1da7456b7e1e0a
chardso <wafelaandrew2@gmail.com>
2022-12-16 03:35:54 +0300

    task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,23 +1,48 @@
-#include "main.h"
+#include "chardso.h"
+
 /**
-* print_number - prints an integer
-* @n:.input integer parameter
-*
-*created by chardso/andrew makerspace
+* print_number - Function that prints an integer.
+* @n: int type number
+* Description: Can only use _putchar to print.
 */
 void print_number(int n)
 {
-unsigned int i = n;
+long m; /* power of 10 */
+int c; /* boolean check */
+long num; /* convert int to long */
 
-if (n < 0)
+num = n;
+/* negatives */
+if (num < 0)
 {
-_putchar(45);
-i = -i;
+num *= -1;
+_putchar('-');
 }
-if (i / 10)
+
+/* count up */
+m = 1;
+c = 1;
+while (c)
 {
-print_number(i / 10);
+if (num / (m * 10) > 0)
+m *= 10;
+else
+c = 0;
 }
-_putchar(i % 10 + '0');
+
+/* count down */
+while (num >= 0)
+{
+if (m == 1)
+{
+_putchar(num % 10 + '0');
+num = -1;
+}
+else
+{
+_putchar((num / m % 10) + '0');
+m /= 10;
+}
+}
 }
 

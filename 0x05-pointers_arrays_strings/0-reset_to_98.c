6521ea8a1f146b67713d979c7a65c37039522ef4
chardso <wafelaandrew2@gmail.com>
2022-12-20 00:22:36 +0300

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* reset_to_98 - resets the value at the memory address at parameter address
+* and replaces it will 98
+* @p: is a pointer variable
+*/
+void reset_to_98(int *p)
+{
+*p = 98;
+
+}
+

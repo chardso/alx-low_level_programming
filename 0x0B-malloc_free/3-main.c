3726b70e53c02731ec1c88408fa72047d0f6f188
chardso <wafelaandrew2@gmail.com>
2023-01-09 15:40:35 +0300

    task 2

--- 0x0B-malloc_free/3-main.c
+++ 0x0B-malloc_free/3-main.c
@@ -37,17 +37,17 @@ int main(void)
 {
 int **grid;
 
-grid = alloc_grid(48, 32);
+grid = alloc_grid(6, 4);
 if (grid == NULL)
 {
-Return(1);
+return (1);
 }
-print_grid(grid, 48, 32);
+print_grid(grid, 6, 4);
 printf("\n");
 grid[0][3] = 98;
 grid[3][4] = 402;
 print_grid(grid, 6, 4);
-Return(0);
+return (0);
 }
 
 

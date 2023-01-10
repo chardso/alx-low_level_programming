9744e86d8096a9e338db871996bc8ab6c5cce19b
chardso <wafelaandrew2@gmail.com>
2023-01-11 01:50:45 +0300

    task 0

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _strlen- takes in a string of characters and return the string length
+* finding length of a string using strlen from string.h
+* @s: input parameter
+* len - returns the integer length of string of characters in s
+* Return: length of string
+*
+*/
+int _strlen(char *s)
+{
+int len;
+for (len = 0; s[len] != 0; len++)
+				{}
+return (len);
+}
+
---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* _puts - takes in string and prints it out
+* @str: pointer variable
+*/
+void _puts(char *str)
+{
+printf("%s\n", str);
+}
+
